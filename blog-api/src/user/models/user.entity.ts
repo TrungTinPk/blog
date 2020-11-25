996949af77b01a388853eda3fc409e6a48008d13
Kevin Tran <tinttran@kpmg.com.vn>
2020-11-25 10:37:02 +0700

    create user feature module, with userService and userController, added postman requests the userEntity and userInterface

---
+++ blog-api/src/user/models/user.entity.ts
@@ -0,0 +1,13 @@
+﻿import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity()
+export class  UserEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+    
+    @Column()
+    name: string;
+    
+    @Column({unique: true})
+    username: string;
+}
